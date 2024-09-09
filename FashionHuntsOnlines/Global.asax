19b209a2ec7181f39f049a75618c301a988119af
Cocomo <Cocomo@DESKTOP-V8FJC1F>
2024-09-09 18:02:12 +0500

    Add project files.

---
+++ FashionHuntsOnlines/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FashionHuntsOnlines.MvcApplication" Language="C#" %>
---
+++ FashionHuntsOnlines/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FashionHuntsOnlines.Startup))]
+namespace FashionHuntsOnlines
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
